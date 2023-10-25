ae8a1366d7c7e3d69ce617d9b699c20eb1fd8aef
fahrezapratamh <fahrezaxgamer@gmail.com>
2023-10-25 06:10:35 -0700

    fix beberapa bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://fahreza-pratama-hidayat-fahreza-pratama-hidayat-s-projects.vercel.app/',
   plugins: [react()],
 })
